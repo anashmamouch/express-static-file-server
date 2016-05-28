bd8682ce9683a11a69b47273ed417a6fcbec616e
anashmamouch <anashmamouch90@gmail.com>
2016-05-28 17:34:53 +0100

    using morgan and express.static middleware

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 4000;
 
 var app = express();
 
 //logs all incoming requests
-app.use(function(req, res, next){
-  console.log("IP: " + req.url);
-  console.log("date: " + new Date());
-  next();
-});
+app.use(morgan("short"));
+
+var filePath = path.join(__dirname, "static");
 
 //static file server middleware
-app.use(function(req, res, next){
-  var filePath = path.join(__dirname, "static", req.url);
-  fs.stat(filePath, function(err, fileInfo){
-    if(err){
-      next();
-      return;
-    }
-
-    if(fileInfo.isFile()){
-      res.sendFile(filePath);
-    }else{
-      next();
-    }
-  })
-});
+app.use(express.static(filePath));
 
 //404 error handler
 app.use(function(req, res){
